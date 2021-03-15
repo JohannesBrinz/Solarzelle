3cc55e35084c2ef7e6cc9118b5db36f8b363412f
Schwannes <johannesbrinz@web.de>
2021-03-15 17:50:58 +0100

    Das ist mein Code.

--- Auswertung.py
+++ Auswertung.py
@@ -16,49 +16,49 @@ from scipy.stats import norm
 
 
 #Datenimport
-Kippunng = pd.read_csv("Vanelli Brinz 12-03-21/Kippung.dat", sep = "\t\t", header = 0, \
+Kippunng = pd.read_csv("Daten/Kippung.dat", sep = "\t\t", header = 0, \
     names = ["Winkel[rad]", "I_K,o[yA]", "I_K,ano[mA]"], engine='python')    #40 < T [C] < 46
-Leerlaufspannung = pd.read_csv("Vanelli Brinz 12-03-21/Leerlaufspannungen.dat", sep = "\t", header = 0, \
+Leerlaufspannung = pd.read_csv("Daten/Leerlaufspannungen.dat", sep = "\t", header = 0, \
     names = ["T[C]", "U_L[mV]"])
-a_an_11mV = pd.read_csv("Vanelli Brinz 12-03-21/a_an_11mV.dat", sep = "\t", header = 8, \
+a_an_11mV = pd.read_csv("Daten/a_an_11mV.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-a_an_du = pd.read_csv("Vanelli Brinz 12-03-21/a_an_du.dat", sep = "\t", header = 8, \
+a_an_du = pd.read_csv("Daten/a_an_du.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-a_org2_11 = pd.read_csv("Vanelli Brinz 12-03-21/a_org2_11.dat", sep = "\t", header = 8, \
+a_org2_11 = pd.read_csv("Daten/a_org2_11.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-a_org2_du = pd.read_csv("Vanelli Brinz 12-03-21/a_org2_du.dat", sep = "\t", header = 8, \
+a_org2_du = pd.read_csv("Daten/a_org2_du.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-a_org_11mV = pd.read_csv("Vanelli Brinz 12-03-21/a_org_11mV.dat", sep = "\t", header = 8, \
+a_org_11mV = pd.read_csv("Daten/a_org_11mV.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-a_org_du = pd.read_csv("Vanelli Brinz 12-03-21/a_org_du.dat", sep = "\t", header = 8, \
+a_org_du = pd.read_csv("Daten/a_org_du.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-a_si_11mV = pd.read_csv("Vanelli Brinz 12-03-21/a_si_11mV.dat", sep = "\t", header = 8, \
+a_si_11mV = pd.read_csv("Daten/a_si_11mV.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-a_si_du = pd.read_csv("Vanelli Brinz 12-03-21/a_si_du.dat", sep = "\t", header = 8, \
+a_si_du = pd.read_csv("Daten/a_si_du.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-b_5 = pd.read_csv("Vanelli Brinz 12-03-21/b_5.dat", sep = "\t", header = 8, \
+b_5 = pd.read_csv("Daten/b_5.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-b_20 = pd.read_csv("Vanelli Brinz 12-03-21/b_20.dat", sep = "\t", header = 8, \
+b_20 = pd.read_csv("Daten/b_20.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-b_sonne = pd.read_csv("Vanelli Brinz 12-03-21/b_sonne.dat", sep = "\t", header = 8, \
+b_sonne = pd.read_csv("Daten/b_sonne.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-c1_11mV = pd.read_csv("Vanelli Brinz 12-03-21/c1_11mV.dat", sep = "\t", header = 8, \
+c1_11mV = pd.read_csv("Daten/c1_11mV.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-c3_v1_11 = pd.read_csv("Vanelli Brinz 12-03-21/c3_v1_11.dat", sep = "\t", header = 8, \
+c3_v1_11 = pd.read_csv("Daten/c3_v1_11.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-c3_v2_11 = pd.read_csv("Vanelli Brinz 12-03-21/c3_v2_11.dat", sep = "\t", header = 8, \
+c3_v2_11 = pd.read_csv("Daten/c3_v2_11.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-c3_v3_11 = pd.read_csv("Vanelli Brinz 12-03-21/c3_v3_11.dat", sep = "\t", header = 8, \
+c3_v3_11 = pd.read_csv("Daten/c3_v3_11.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-c4_11verb = pd.read_csv("Vanelli Brinz 12-03-21/c4_11verb.dat", sep = "\t", header = 8, \
+c4_11verb = pd.read_csv("Daten/c4_11verb.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-d_30 = pd.read_csv("Vanelli Brinz 12-03-21/d_30.dat", sep = "\t", header = 8, \
+d_30 = pd.read_csv("Daten/d_30.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-r68_s2 = pd.read_csv("Vanelli Brinz 12-03-21/r68_s2.4.dat", sep = "\t", header = 8, \
+r68_s2 = pd.read_csv("Daten/r68_s2.4.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-r68_s3 = pd.read_csv("Vanelli Brinz 12-03-21/r68_s3.6.dat", sep = "\t", header = 8, \
+r68_s3 = pd.read_csv("Daten/r68_s3.6.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
-r73_s2 = pd.read_csv("Vanelli Brinz 12-03-21/r73_s2,6.dat", sep = "\t", header = 8, \
+r73_s2 = pd.read_csv("Daten/r73_s2,6.dat", sep = "\t", header = 8, \
     names = ["voltage / V", "current / A", "time"])
 
 
@@ -75,10 +75,10 @@ params, params_cov = optimize.curve_fit(U, a_si_du["current / A"][0:970],\
  a_si_du["voltage / V"][0:970])
 
 #Plot
-plt.errorbar(a_si_du["voltage / V"][0:970], a_si_du["current / A"][0:970], xerr = 0, yerr = 0,\
+plt.errorbar(a_si_du["voltage / V"][400:970], a_si_du["current / A"][400:970], xerr = 0, yerr = 0,\
             linewidth = 2, color = "green", capsize=3)
-plt.errorbar(U(np.linspace(-1, 1, 1000), params[0], params[1], params[2]), np.linspace(-1, 1, 1000), \
- lw=1, fmt = "--", c = "black")
+#plt.errorbar(U(np.linspace(-1, 1, 1000), params[0], params[1], params[2]), np.linspace(-1, 1, 1000), \
+ #lw=1, fmt = "--", c = "black")
 #plt.errorbar(np.linspace(0, 0.5, 1000), np.linspace((1/2.718),(1/2.718), 1000))
 plt.title('U-I Kennlinie Si dunkel', fontsize = 15)
 #plt.text( 0, 0,"$L_c = $" + str(round(t_c * 3*10e8, 3)))
